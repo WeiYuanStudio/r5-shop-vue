2adbbabf33e94673ee5461a76ed623cc3ab46d55
neruuuuuuu <906413245@qq.com>
2020-09-08 20:11:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <PageOrder />
+  </div>
+</template>
+
+<script>
+import PageOrder from "./components/PageOrder.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageOrder,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PageOrder.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <mt-field label="昵称：" placeholder v-model="name"></mt-field>
+    <mt-field label="房号：" type="number" placeholder v-model="room"></mt-field>
+    <mt-field label="电话：" type="number" placeholder v-model="tele"></mt-field>
+
+    <mt-button type="default">提交订单</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      room: "",
+      tele: "",
+    };
+  },
+  methods: {
+    submit() {},
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
