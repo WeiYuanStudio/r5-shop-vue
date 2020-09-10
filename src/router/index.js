cc7ed4468455ed34844b47ef47c7f87b5a013e2f
neruuuuuuu <906413245@qq.com>
2020-09-10 18:12:36 +0800

    路由添加页面

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import PageHome from '@/pages/PageHome.vue'
 import PageCart from '@/pages/PageCart.vue'
 import PageOrder from '@/pages/PageOrder.vue'
 import PageAbout from '@/pages/PageAbout.vue'
+import FormOrder from '@/pages/FormOrder.vue'
+import OrderDetails from '@/pages/OrderDetails.vue'
 
 Vue.use(Router)
 
@@ -14,6 +16,8 @@ export default new Router({
         { path: '/home', component: PageHome },
         { path: '/cart', component: PageCart },
         { path: '/order', component: PageOrder },
-        { path: '/about', component: PageAbout }
+        { path: '/about', component: PageAbout },
+        { path: '/form', component: FormOrder },
+        { path: '/details', component: OrderDetails },
     ]
 })
