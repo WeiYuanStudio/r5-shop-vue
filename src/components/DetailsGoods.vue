8bd1d7ad346364638320eb79a59a3ce4346ef995
neruuuuuuu <906413245@qq.com>
2020-09-10 23:46:51 +0800

    增加订单详情页

---
+++ src/components/DetailsGoods.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="good-info">
+    <div class="good-info-left">
+      <img v-lazy="imgLink" />
+    </div>
+    <!---->
+    <div class="good-info-right">
+      <div class="good-info-right-up">
+        <div class="good-name">{{name}}</div>
+        <div class="good-price">￥{{price}}</div>
+      </div>
+      <div class="good-info-right-down">数量</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["goodsInfo"],
+  data() {
+    return {
+      ...this.goodsInfo,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+.good-info {
+  margin: 8px 5px;
+  display: flex;
+  flex-flow: row;
+}
+
+.good-info-right {
+  flex: auto;
+  margin: 1px 10px;
+  display: flex;
+  flex-flow: column;
+}
+
+.good-info-right-up {
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.good-info-right-down-info {
+  text-align: left;
+}
+</style>
--- src/pages/OrderDetails.vue
+++ src/pages/OrderDetails.vue
@@ -1,11 +1,74 @@
 <template>
-  <div></div>
+  <div>
+    <div class="order-details-page">
+      <div class="order-details-state">状态</div>
+      <div class="order-details-goods">
+        商品
+        <div v-for="(goods, index) in goodsList" :key="index">
+          <DetailsGoods :goodsInfo="goods" />
+        </div>
+      </div>
+      <div class="order-details-user">
+        <div class="order-details-user-name">昵称</div>
+        <div class="order-details-user-room">房号</div>
+        <div class="order-details-user-tele">手机号</div>
+      </div>
+      <div class="order-details-order">
+        <div class="order-details-order-number">订单号</div>
+        <div class="order-details-order-mode">支付方式</div>
+        <div class="order-details-order-time">下单时间</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import DetailsGoods from "@/components/DetailsGoods.vue";
+
+export default {
+  components: {
+    DetailsGoods,
+  },
+  data() {
+    return {
+      goodsList: [
+        {
+          id: 1,
+          imgLink:
+            "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
+          name: "YOUR NAME1",
+          price: "100",
+        },
+        {
+          id: 2,
+          imgLink:
+            "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
+          name: "YOUR NAME2",
+          price: "100",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.order-details-page {
+  padding: 5px 0;
+  text-align: left;
+}
+
+.order-details-state,
+.order-details-goods,
+.order-details-user,
+.order-details-order {
+  margin: 8px 4px;
+  padding: 8px;
+
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.15);
+  transition: all 0.1s ease-in;
+}
 </style>
