04deeff54ac91d1bc0bbd8d6860e172691753105
Yuan Wei <weiyuanstudio@stu.jluzh.edu.cn>
2021-06-07 18:32:34 +0800

    update vant ui for nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,12 @@
 <template>
   <div>
-    <mt-tabbar v-model="selected">
-      <mt-tab-item id="home">
-        <font-awesome-icon class="router-icon" icon="home" fixed-width />
-        <div class="router-font">主页</div>
-      </mt-tab-item>
-      <mt-tab-item id="cart">
-        <font-awesome-icon class="router-icon" icon="shopping-cart" fixed-width />
-        <div class="router-font">购物车</div>
-      </mt-tab-item>
-      <mt-tab-item id="order">
-        <font-awesome-icon class="router-icon" icon="list" fixed-width />
-        <div class="router-font">订单</div>
-      </mt-tab-item>
-      <mt-tab-item id="about">
-        <font-awesome-icon class="router-icon" icon="info" fixed-width />
-        <div class="router-font">关于</div>
-      </mt-tab-item>
-    </mt-tabbar>
-    <div v-show="cartCount!=0">
-      <mt-badge size="small" color="red" class="cart-count-badge">{{cartCount}}</mt-badge>
-    </div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o" to="/home">主页</van-tabbar-item>
+      <van-tabbar-item name="show" icon="newspaper-o">买家秀</van-tabbar-item><!-- Todo: -->
+      <van-tabbar-item name="cart" icon="shopping-cart-o" to="/cart" :badge="cartCount">购物车</van-tabbar-item>
+      <van-tabbar-item name="order" icon="orders-o" to="/order">订单</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="user-o" to="/about">我的</van-tabbar-item><!-- Todo: -->
+    </van-tabbar>
   </div>
 </template>
 
@@ -28,7 +14,7 @@
 export default {
   data() {
     return {
-      selected: null,
+      active: null
     };
   },
   computed: {
@@ -36,25 +22,5 @@ export default {
       return this.$store.getters.getCartCount;
     },
   },
-  watch: {
-    selected(val) {
-      /* 通过id来产生router url，请保证id与路由url一致 */
-      this.$router.push("/" + val);
-    },
-  },
 };
 </script>
-
-<style scoped>
-.router-icon {
-  font-size: 20px;
-  margin: 4px;
-}
-
-.cart-count-badge {
-  position: fixed;
-  left: 40%;
-  bottom: 30px;
-  z-index: 50;
-}
-</style>
