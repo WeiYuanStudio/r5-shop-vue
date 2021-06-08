f12705d786f5baa249f7be76548bab08eb6f7c7f
Yuan Wei <weiyuanstudio@stu.jluzh.edu.cn>
2021-06-08 16:52:29 +0800

    add buyer show post

--- src/pages/PageAddBuyerShow.vue
+++ src/pages/PageAddBuyerShow.vue
@@ -1,34 +1,60 @@
 <template>
   <div>
     <div>
-    <van-nav-bar
-      title="创建我的买家秀"
-      left-text="返回"
-      left-arrow
-      @click-left="$router.back()"
-    />
+      <van-nav-bar
+          title="创建我的买家秀"
+          left-text="返回"
+          left-arrow
+          @click-left="$router.back()"
+      />
     </div>
     <div>
-      <van-cell-group>
-        <van-field v-model="title" label="标题" placeholder="请输入标题" />
-        <van-field v-model="content" type="textarea" autosize label="内容" placeholder="请输入内容"/>
-      </van-cell-group> 
-      <br/>
-      <van-button type="primary">提交</van-button>
+      <van-form @submit="submit">
+        <van-field
+            label="标题"
+            name="title"
+            placeholder="请输入标题"
+
+            v-model="title"
+        />
+        <van-field
+            label="内容"
+            name="content"
+            type="textarea"
+            placeholder="请输入内容"
+
+            autosize
+            v-model="content"
+        />
+        <div style="margin: 16px;">
+          <van-button round block type="info" native-type="submit">提交</van-button>
+        </div>
+      </van-form>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import {Toast} from "vant";
+
 export default {
   data() {
     return {
-      title : '',
-      content : '',
+      title: '',
+      content: '',
     };
   },
-  methods:{
-    
+  methods: {
+    submit(val) {
+      Toast.loading()
+      console.log(val)
+      axios.post("/api/buyer-show/", val).then(() => {
+        Toast.success("提交成功")
+      }).catch(() => {
+        Toast.fail("提交失败")
+      })
+    }
   }
 };
 </script>
