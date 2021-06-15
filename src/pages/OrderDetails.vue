21e306c56d335b534818e477cac4d0b027b31566
WeiYuanStudio <weiyuanstudio@gmail.com>
2021-06-15 15:59:05 +0800

    增加详情页title

--- src/pages/OrderDetails.vue
+++ src/pages/OrderDetails.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
+    <van-nav-bar
+      title="订单详情"
+      left-text="返回"
+      left-arrow
+      @click-left="$router.back()"
+    />
     <div class="order-details-page">
-      <div class="order-details-state">状态: {{ tranState(goodsList.state) }}</div>
+      <div class="order-details-state">
+        状态: {{ tranState(goodsList.state) }}
+      </div>
       <div class="order-details-goods">
         商品
-        <div v-for="(item,i) in goods" :key="i">
-          <DetailsGoods :goodsInfo="item"/>
+        <div v-for="(item, i) in goods" :key="i">
+          <DetailsGoods :goodsInfo="item" />
         </div>
       </div>
       <div class="order-details-user">
@@ -15,16 +23,20 @@
       </div>
       <div class="order-details-order">
         <div class="order-details-order-number">订单号: {{ goodsList.id }}</div>
-        <div class="order-details-order-mode">支付方式: {{ goodsList.mode }}</div>
-        <div class="order-details-order-time">下单时间: {{ goodsList.time }}</div>
+        <div class="order-details-order-mode">
+          支付方式: {{ goodsList.mode }}
+        </div>
+        <div class="order-details-order-time">
+          下单时间: {{ goodsList.time }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import DetailsGoods from "@/components/DetailsGoods.vue";
 import axios from "axios";
+import DetailsGoods from "@/components/DetailsGoods.vue";
 
 export default {
   components: {
@@ -36,14 +48,14 @@ export default {
         {
           id: 1,
           imgLink:
-              "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
+            "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
           name: "YOUR NAME1",
           price: "100",
         },
         {
           id: 2,
           imgLink:
-              "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
+            "https://m.media-amazon.com/images/I/51TfrooMUWL._SL160_.jpg",
           name: "YOUR NAME2",
           price: "100",
         },
@@ -53,24 +65,21 @@ export default {
   },
   methods: {
     tranState(state) {
-      if (state === 0)
-        return "待配送"
-      if (state === 1)
-        return "配送中"
-      if (state === 2)
-        return "完成"
-    }
+      if (state === 0) return "待配送";
+      if (state === 1) return "配送中";
+      if (state === 2) return "完成";
+    },
   },
   mounted() {
-    this.goodsList = this.$route.query.list
-    let keys = Object.keys(this.goodsList.good)
+    this.goodsList = this.$route.query.list;
+    let keys = Object.keys(this.goodsList.good);
     for (let i = 0; i < keys.length; i++) {
-      axios.get("/api/good/" + keys[i]).then(resp => {
-        resp.data.num = this.goodsList.good[keys[i]]
-        this.goods.push(resp.data)
-      })
+      axios.get("/api/good/" + keys[i]).then((resp) => {
+        resp.data.num = this.goodsList.good[keys[i]];
+        this.goods.push(resp.data);
+      });
     }
-  }
+  },
 };
 </script>
 
