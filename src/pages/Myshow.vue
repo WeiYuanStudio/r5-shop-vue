5c61c18ff06e54a9e5db197a47d060fedde27ec8
neruuuuuuu <906413245@qq.com>
2021-06-15 18:10:18 +0800

    我的买家秀删除功能

--- src/pages/Myshow.vue
+++ src/pages/Myshow.vue
@@ -3,13 +3,13 @@
    <van-nav-bar
        title="我的买家秀"
        left-arrow
-       @click-left="$router.back()"
+       @click-left="$router.push('/buyersShow')"
        right-text="新增"
        @click-right="$router.push('/addBuyerShow')"
    />
    <div v-if="buyersShowList.length>0">
      <div :key="index" v-for="(buyersShow, index) in buyersShowList">
-       <ItemBuyersShow :buyersShow="buyersShow" />
+       <ItemBuyersShow :buyersShow="buyersShow" :deletable="true" @refresh="onRefresh" />
      </div>
    </div>
    <div v-else>
@@ -34,20 +34,25 @@ export default {
       buyersShowList: [],
     };
   },
-  created() {
-
-    Toast.loading()
-    if (this.$store.getters.isLogin) {
+  methods: {
+    onRefresh() {
+      Toast.loading()
       axios.get("/api/users/get_self_user_info/").then(resp => {
         this.user = resp.data
-        axios.get("/api/buyer-show/?customer="+ this.user.id).then((resp) => {
+        // axios.get("/api/buyer-show/?customer="+ this.user.id).then((resp) => {
+        //   this.buyersShowList = resp.data.results;
+        //   Toast.clear()
+        // });
+
+        axios.get("/api/buyer-show/", {params: {customer: this.user.id}}).then((resp) => {
           this.buyersShowList = resp.data.results;
           Toast.clear()
         });
       })
-
     }
-
+  },
+  created() {
+    this.onRefresh()
   },
 }
 </script>
